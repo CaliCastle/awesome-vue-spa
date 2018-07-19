e7a906af50e6aefabb03dec11ba1df2689ebb0d4
Cali Castle <cali@calicastle.com>
2018-07-19 01:33:35 -0700

    Add channel list ui

--- resources/assets/js/views/home/Channels.vue
+++ resources/assets/js/views/home/Channels.vue
@@ -1,11 +1,48 @@
 <template>
     <div class="column">
-
+        <nav class="panel">
+            <p class="panel-heading">
+                repositories
+            </p>
+            <div class="panel-block">
+                <p class="control has-icons-left">
+                    <input class="input is-small" type="text" placeholder="search">
+                    <span class="icon is-small is-left">
+        <i class="fas fa-search" aria-hidden="true"></i>
+      </span>
+                </p>
+            </div>
+            <a class="panel-block is-active">
+                全部频道
+            </a>
+            <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+                频道1
+            </a>
+            <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+                频道2
+            </a>
+            <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-code-branch" aria-hidden="true"></i>
+    </span>
+                频道3
+            </a>
+            <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-code-branch" aria-hidden="true"></i>
+    </span>
+                频道4
+            </a>
+        </nav>
     </div>
 </template>
 
 <script>
-    export default {
-
-    }
+    export default {}
 </script>
